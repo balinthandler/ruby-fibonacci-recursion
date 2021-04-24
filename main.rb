1e2e9d376c28ee98c9bd31218121a7e536507a8f
Balint Handler <handlerbalint@gmail.com>
2021-04-24 16:03:42 +0200

    recursive method added

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-#with regular loop
+# with regular loop
+
 def fibs(num)
   sequence = []
   num.times do |i|
     if i > 1
-      elem = sequence[i-1] + sequence[i-2]
+      elem = sequence[i - 1] + sequence[i - 2]
       sequence << elem
     else
       sequence << i
@@ -11,6 +12,16 @@ def fibs(num)
   end
   sequence
 end
+# with recursion
 
-puts fibs(5)
+def fibs_rec(n)
+  return [0] if n == 0
+  return [0, 1] if n == 1
 
+  array = fibs_rec(n - 1)
+  array << array[-2] + array[-1]
+end
+
+puts fibs_rec(8)
+
+puts fibs(8)
